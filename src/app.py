bbe3e2e0a14826cab798c42a37dfbeb25c4d9057
Brooks Wood <brooks.wood@gmail.com>
2023-03-29 22:53:59 -0600

    initial commit '

--- src/app.py
+++ src/app.py
@@ -2,30 +2,30 @@ from flask import abort, Flask, request
 from functools import wraps
 from twilio.request_validator import RequestValidator
 from twilio.twiml.messaging_response import MessagingResponse
-from twilValidator import validate_twilio_request
+# from twilValidator import validate_twilio_request
 import os
 
-# def validate_twilio_request(f):
-#     """Validates that incoming requests genuinely originated from Twilio"""
-#     @wraps(f)
-#     def decorated_function(*args, **kwargs):
-#         # Create an instance of the RequestValidator class
-#         validator = RequestValidator(os.environ.get('TWILIO_AUTH_TOKEN'))
+def validate_twilio_request(f):
+    """Validates that incoming requests genuinely originated from Twilio"""
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        # Create an instance of the RequestValidator class
+        validator = RequestValidator(os.environ.get('TWILIO_AUTH_TOKEN'))
 
-#         # Validate the request using its URL, POST data,
-#         # and X-TWILIO-SIGNATURE header
-#         request_valid = validator.validate(
-#             request.url,
-#             request.form,
-#             request.headers.get('X-TWILIO-SIGNATURE', ''))
+        # Validate the request using its URL, POST data,
+        # and X-TWILIO-SIGNATURE header
+        request_valid = validator.validate(
+            request.url,
+            request.form,
+            request.headers.get('X-TWILIO-SIGNATURE', ''))
 
-#         # Continue processing the request if it's valid, return a 403 error if
-#         # it's not
-#         if request_valid:
-#             return f(*args, **kwargs)
-#         else:
-#             return abort(403)
-#     return decorated_function
+        # Continue processing the request if it's valid, return a 403 error if
+        # it's not
+        if request_valid:
+            return f(*args, **kwargs)
+        else:
+            return abort(403)
+    return decorated_function
 
 app = Flask(__name__)
 
