b9c201f66f3e47ccd8324c49792ce9a9395a24ef
Brooks Wood <brooks.wood@gmail.com>
2023-03-29 23:28:20 -0600

    initial commit '

--- src/app.py
+++ src/app.py
@@ -1,40 +1,36 @@
 from flask import abort, Flask, request
-# from functools import wraps
+from functools import wraps
 from twilio.request_validator import RequestValidator
-from twilio.twiml.messaging_response import MessagingResponse
+from twilio.twiml.voice_response import VoiceResponse, Conference, Dial, Say, Gather, Record, Leave, Hangup, Pay, Prompt, Connect
 # from twilValidator import validate_twilio_request
 import os
 
-# def validate_twilio_request(f):
-#     """Validates that incoming requests genuinely originated from Twilio"""
-#     @wraps(f)
-#     def decorated_function(*args, **kwargs):
-#         # Create an instance of the RequestValidator class
-#         validator = RequestValidator(os.environ.get('TWILIO_AUTH_TOKEN'))
+TWILIO_AUTH_TOKEN = os.getenv('TWILIO_AUTH_TOKEN')
 
-#         # Validate the request using its URL, POST data,
-#         # and X-TWILIO-SIGNATURE header
-#         request_valid = validator.validate(
-#             request.url,
-#             request.form,
-#             request.headers.get('X-TWILIO-SIGNATURE', ''))
-
-#         # Continue processing the request if it's valid, return a 403 error if
-#         # it's not
-#         if request_valid:
-#             return f(*args, **kwargs)
-#         else:
-#             return abort(403)
-#     return decorated_function
+def validate_twilio_request():
+    def extra(f):
+        @wraps(f)
+        def decorated(*args, **kwargs):
+            validator = RequestValidator(TWILIO_AUTH_TOKEN)
+            https_url = 'https://' + request.url.lstrip('http://') # with ngrok, request.url shows http when https is used, so we need to fix it
+            twilio_signature = request.headers.get('X-Twilio-Signature')
+            params = request.form
+            if not twilio_signature:
+                return Response('No signature', 400)  
+            elif not validator.validate(https_url, params, twilio_signature):
+                return Response('Incorrect signature', 403)
+            return f(*args, **kwargs)
+        return decorated
+    return extra
 
 app = Flask(__name__)
 
 @app.route("/sms", methods=['GET', 'POST'])
-# @validate_twilio_request
+@validate_twilio_request()
 def sms_reply():
-    validator = RequestValidator(os.environ.get('TWILIO_AUTH_TOKEN'))
-    if not validator.validate(request.url, request.form, request.headers.get('X-Twilio-Signature')):
-        abort(400)
+    # validator = RequestValidator(os.environ.get('TWILIO_AUTH_TOKEN'))
+    # if not validator.validate(request.url, request.form, request.headers.get('X-Twilio-Signature')):
+    #     abort(400)
 
     """Respond to incoming calls with a MMS message."""
     # Start our TwiML response
