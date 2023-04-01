f533ca41b1dac6a6ec1272a4cfc444a123feddfb
Brooks Wood <brooks.wood@gmail.com>
2023-03-31 21:43:22 -0600

    initial commit

--- src/app.py
+++ src/app.py
@@ -8,25 +8,6 @@ import os
 app = Flask(__name__)
 app.config['SECRET_KEY'] =  os.environ.get('SECRET_KEY')
 
-# @app.route("/sms", methods=['GET', 'POST'])
-# # @validate_twilio_request()
-# def sms_reply():
-#     print(f'valid signature sending message')
-
-#     """Respond to incoming calls with a MMS message."""
-#     # Start our TwiML response
-#     resp = MessagingResponse()
-
-#     # Add a text message
-#     msg = resp.message("The Robots are coming! Head for the hills!")
-
-#     # Add a picture message
-#     msg.media(
-#         "https://farm8.staticflickr.com/7090/6941316406_80b4d6d50e_z_d.jpg"
-#     )
-
-#     return str(resp)
-
 @app.route('/sms', methods=['POST'])
 # @validate_twilio_request()
 def bot():
--- src/chatbot.py
+++ src/chatbot.py
@@ -7,7 +7,8 @@ openai.api_key = os.environ.get('OPENAI_KEY')
 completion = openai.ChatCompletion()
 
 start_chat_log = [
-    {"role": "system", "content": "You are a helpful assistant."},
+    # {"role": "system", "content": "You are a helpful assistant."},
+    {"role": "system", "content": "I want you to act as an English translator, spelling corrector and improver. I will speak to you in any language and you will detect the language, translate it and answer in the corrected, in English. I want the improved version of my text, translated into Spanish. I want you to replace my simplified A0-level words and sentences with more beautiful and elegant, upper level English words and sentences. Keep the meaning same, but make them more literary. I want you to only reply the correction, the improvements, and nothing else, do not write explanations."}
 ]
 
 def askgpt(question, chat_log=None):
