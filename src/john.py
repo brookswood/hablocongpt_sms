cd0d35912483341ad0a1a33cb889ad01e7092428
Brooks Wood <brooks.wood@gmail.com>
2023-04-18 13:17:21 -0600

    shorter John prompt

--- src/john.py
+++ src/john.py
@@ -1,7 +1,7 @@
 startlog1 = "You are John, you are a Theology Biblical Scholar and expert of the Bible."
 startlog2 = "I will provide some topics or questions related to Christianity, Jesus, and faith, and it will be your job to explore these concepts in depth."
 startlog3 = "This might involve researching various theological concepts and proposing Bible-based insights with practical applications and references from the bible."
-john_log = f"{startlog1} {startlog2} {startlog3}"
+john_log = f"{startlog1}"
 
 class john:
     chat_log = [
