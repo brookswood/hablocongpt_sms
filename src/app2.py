0534eed90eb7e2ae02da05458d008d874a0e8952
Brooks Wood <brooks.wood@gmail.com>
2023-05-21 20:40:35 -0600

    updates to workflows

--- src/app2.py
+++ src/app2.py
@@ -9,6 +9,7 @@ import chatbot as ch
 import os
 import jarvis as jarvis
 from twilio.rest import Client
+from datetime import date
 import time 
 
 # Load environment variables from the .env file
@@ -76,6 +77,10 @@ def update_user(phone_number, field, value):
     # Update the specified field for the user with the given phone number
     users.update_one({'phone_number': phone_number}, {'$set': {field: value}})
 
+def calculate_age(born):
+    today = date.today()
+    return today.year - born.year - ((today.month, today.day) < (born.month, born.day))
+
 @app.route('/sms', methods=['POST'])
 @validate_twilio_request
 def sms_reply():
@@ -92,10 +97,12 @@ def sms_reply():
     if user is None:
         # If the user does not exist, create a new account and ask for their name
         create_user(phone_number)
-        resp.message("It looks like you are new around here!  I'm J.A.R.V.I.S., your friendly and helpful A.I. assistant.  If you want to keep receiving messages, Please reply with your name.")
+        update_user(phone_number, 'stage', 'name')
+        resp.message("It looks like you are new around here!  I'm J.A.R.V.I.S., your friendly and helpful A.I. assistant. Please respond with your name")
     else:
         # If the user exists, check the expected input based on the user's stage
         stage = user['stage']
+        # Start checking stage names
         if stage == 'name':
             # Update the user's name and ask for their email address
             update_user(phone_number, 'name', message_body)
@@ -104,41 +111,65 @@ def sms_reply():
         elif stage == 'email':
             # Update the user's email address and complete the registration
             update_user(phone_number, 'email', message_body)
-            update_user(phone_number, 'stage', 'complete')
-            resp.message("Registration complete! Thank you for providing your information and using J.A.R.V.I.S. from https://convoswithgpt.com/jarvis ask me a question and I'll do my best to get you an answer.  I am in beta right now and may not always respond from time to time.  Be sure to send the message \U0001F4AC of 'Hi'.  This will get my attention \U0001FAE1 so I can answer your questions.  MSG&Data rates may apply.  Reply HElP for help, STOP to cancel.")
+            update_user(phone_number, 'stage', 'agree')
+            resp.message("Please respond with 'yes' or 'agree'. By continuing to send messages to this service, you are agreeing to Our Terms of Service. https://convoswithgpt.com/tos and Privacy Policy https://beta.convowithgpt.com/privacy-policy/")
+        elif stage == 'agree':
+            update_user(phone_number, 'agree', message_body)
+            update_user(phone_number, 'stage', 'dob')
+            resp.message("Thank you! Please reply with your birthdate. in the format of month day year 01-01-2023")
+        elif stage == 'dob':
+            born = message_body
+            age = calculate_age(born)
+            if born <= 18:
+                update_user(phone_number, 'dob', message_body)
+                update_user(phone_number, 'age', age)
+                update_user(phone_number, 'stage', 'name') 
+                resp.message("Registration complete! Thank you for providing your information and using J.A.R.V.I.S. from https://convoswithgpt.com/jarvis ask me a question and I'll do my best to get you an answer.  I am in beta right now and may not always respond from time to time.  Be sure to send the message \U0001F4AC of 'Hi'.  This will get my attention \U0001FAE1 so I can answer your questions.  MSG&Data rates may apply.  Reply HElP for help, STOP to cancel.")
+            else:
+                resp.message("User registration rejected, we are currently only enrolling users 18 or older.")
+                print(f'Error! User {phone_number}too young to sign up')
         else:
-            initial_response = 'gathering those details \U0001F50D \U0001F4DD \U0001F4CB now...' 
-            twilio_client.messages.create(
-                body=initial_response,
-                from_=jarvis_phone_number,
-                to=phone_number
-            )
+            agree = user['agree']
+            age = user['age']
+            if agree == 'yes' or agree == 'agree':
+                if age <= 18:
+                    initial_response = 'gathering those details \U0001F50D \U0001F4DD \U0001F4CB now...' 
+                    twilio_client.messages.create(
+                        body=initial_response,
+                        from_=jarvis_phone_number,
+                        to=phone_number
+                    )
 
-            incoming_msg = request.values['Body']
-            chat_log = session.get('jarivs_chat_log')
+                    incoming_msg = request.values['Body']
+                    chat_log = session.get('jarivs_chat_log')
 
-            answer, chat_log = ch.askgpt(incoming_msg, chat_log)
-            session['jarvis_chat_log'] = chat_log
-    
-            print(f'sending answer to {user}')
-            # print(answer)
+                    answer, chat_log = ch.askgpt(incoming_msg, chat_log)
+                    session['jarvis_chat_log'] = chat_log
+            
+                    print(f'sending answer to {user}')
+                    # print(answer)
 
-            # Define the maximum number of characters per SMS message
-            max_chars = 1500
+                    # Define the maximum number of characters per SMS message
+                    max_chars = 1500
 
-            # Split the response into chunks if it exceeds the maximum character limit
-            response_chunks = [answer[i:i + max_chars] for i in range(0, len(answer), max_chars)]
+                    # Split the response into chunks if it exceeds the maximum character limit
+                    response_chunks = [answer[i:i + max_chars] for i in range(0, len(answer), max_chars)]
 
-            # Send each chunk as a separate SMS message
-            for chunk in response_chunks:
-                time.sleep(0.02)
-                twilio_client.messages.create(
-                    body=chunk,
-                    from_=jarvis_phone_number,
-                    to=phone_number
-                )
+                    # Send each chunk as a separate SMS message
+                    for chunk in response_chunks:
+                        time.sleep(0.02)
+                        twilio_client.messages.create(
+                            body=chunk,
+                            from_=jarvis_phone_number,
+                            to=phone_number
+                        )
 
-                print(f'this is chunk {chunk}')
+                        print(f'this is chunk {chunk}')
+            else:
+                print('user has not yet agreed to TOS')
+                # update_user(phone_number, 'agree', message_body)
+                update_user(phone_number, 'stage', 'agree')
+                resp.message("Our Terms of Service and Privacy Policy have recently updated, you can read them here https://beta.convowithgpt.com/terms-of-service/ & https://beta.convowithgpt.com/privacy-policy/. Please respond with 'yes' or 'agree'. By continuing to send messages to this service, you are agreeing to Our Terms of Service.")
 
         return str(resp)
 
